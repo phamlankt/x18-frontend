5d3047138cf432f4f9ef86ef8bd2e2fe497a1698
Lan Pham <lan.pham@dfs.de>
2023-10-14 17:05:19 +0200

    adding AlertContext

--- src/App.js
+++ src/App.js
@@ -4,56 +4,60 @@ import AuthState from "./contexts/AuthContext/AuthState";
 import PrivateRoute from "./PrivateRoute";
 import AdminRoute from "./AdminRoute";
 import NotAuthRoute from "./NotAuthRoute";
-import Header from "./components/header/Header";
 import AppState from "./contexts/AppContext/AppState";
 import AdminRecruiterRoute from "./AdminRecruiterRoute";
 import AdminApplicantRoute from "./AdminApplicantRoute";
-import Footer from "./components/footer/Footer";
+import "react-toastify/dist/ReactToastify.css";
+import AlertState from "./contexts/AlertContext/AlertState";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <AppState>
-      <AuthState>
-        <>
-          <Routes>
-            {routes.map((route, index) => {
-              const {
-                path,
-                component,
-                isPrivate,
-                notAuth,
-                isAdmin,
-                isSuperAdmin,
-                isAdminRecruiter,
-                isAdminApplicant,
-              } = route;
-              return (
-                <Route
-                  key={index}
-                  path={path}
-                  element={
-                    isPrivate ? (
-                      isAdmin ? (
-                        <AdminRoute component={component} />
-                      ) : isAdminRecruiter ? (
-                        <AdminRecruiterRoute component={component} />
-                      ) : isAdminApplicant ? (
-                        <AdminApplicantRoute component={component} />
+      <AlertState>
+        <AuthState>
+          <>
+            <ToastContainer />
+            <Routes>
+              {routes.map((route, index) => {
+                const {
+                  path,
+                  component,
+                  isPrivate,
+                  notAuth,
+                  isAdmin,
+                  isSuperAdmin,
+                  isAdminRecruiter,
+                  isAdminApplicant,
+                } = route;
+                return (
+                  <Route
+                    key={index}
+                    path={path}
+                    element={
+                      isPrivate ? (
+                        isAdmin ? (
+                          <AdminRoute component={component} />
+                        ) : isAdminRecruiter ? (
+                          <AdminRecruiterRoute component={component} />
+                        ) : isAdminApplicant ? (
+                          <AdminApplicantRoute component={component} />
+                        ) : (
+                          <PrivateRoute component={component} />
+                        )
+                      ) : notAuth ? (
+                        <NotAuthRoute component={component} />
                       ) : (
-                        <PrivateRoute component={component} />
+                        component
                       )
-                    ) : notAuth ? (
-                      <NotAuthRoute component={component} />
-                    ) : (
-                      component
-                    )
-                  }
-                />
-              );
-            })}
-          </Routes>
-        </>
-      </AuthState>
+                    }
+                  />
+                );
+              })}
+            </Routes>
+          </>
+        </AuthState>
+      </AlertState>
     </AppState>
   );
 }
