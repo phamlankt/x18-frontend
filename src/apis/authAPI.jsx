24f01376dbbc3d6a99fb291598066e06fbf67d54
TrinhLe08 <khanhtrinhmc2021@gmail.com>
2023-10-22 18:58:15 +0700

    n3-26

--- src/apis/authAPI.jsx
+++ src/apis/authAPI.jsx
@@ -3,7 +3,7 @@ import axiosInstance from "./axiosInstance";
 const authAPI = {
   login: (values) => axiosInstance.post("/auth/login", values),
   register: (values) => axiosInstance.post("/auth/register", values),
-  authInfo: () => axiosInstance.get("/auth/me"),
+  authInfo: (values) => axiosInstance.get("/auth/me", values),
 };
 
 export default authAPI;
