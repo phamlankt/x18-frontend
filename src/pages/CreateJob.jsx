78e44d3d8de651d80b3bce80ad452946f5e5905d
NhanDev <nhannguyenvh79@gmail.com>
2023-10-25 16:18:21 +0700

    add update job page

--- src/pages/CreateJob.jsx
+++ src/pages/CreateJob.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MasterLayout from "../components/layout/MasterLayout";
-import CreateJobForm from "../components/creatJob/CreateJobForm";
+import CreateJobForm from "../components/creatJob/JobForm";
 
 function CreateJobPage() {
   return (
