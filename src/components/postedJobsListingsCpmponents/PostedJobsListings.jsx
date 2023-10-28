f06747e07b9bcd4db5809b43e059b856826c9909
TrinhLe08 <khanhtrinhmc2021@gmail.com>
2023-10-28 16:57:53 +0700

    N3-31

--- src/components/postedJobsListingsCpmponents/PostedJobsListings.jsx
+++ src/components/postedJobsListingsCpmponents/PostedJobsListings.jsx
@@ -7,6 +7,10 @@ import { CalendarCheck } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { Button, Modal } from "antd";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Pagination } from "antd";
+import { List } from "antd";
 import applicationAPI from "../../apis/applicationAPI";
 import jobAPI from "../../apis/jobAPI";
 import { Dropdown, Menu } from "antd";
@@ -15,11 +19,21 @@ import { DownOutlined } from "@ant-design/icons";
 const PostedJobsListings = () => {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const [spinConnect, setSpinConnect] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [modalText, setModalText] = useState("Do you want to delete this job?");
   const [dataJob, setDataJob] = useState([]);
   const [idToUpdate, setIdToUpdate] = useState("");
   const [checkDataJob, setCheckDataJob] = useState(true);
+  const antIcon = (
+    <LoadingOutlined
+      style={{
+        fontSize: 50,
+        color: "black",
+      }}
+      spin
+    />
+  );
   if (checkDataJob) {
     jobAPI
       .getListJob()
@@ -36,14 +50,6 @@ const PostedJobsListings = () => {
         console.log(error, 15);
       });
   }
-  const DeleteRequest = (id) => {
-    if (id) {
-      console.log(id);
-    } else {
-      console.log(404);
-      return;
-    }
-  };
 
   const handleMenuClick = (e) => {
     console.log("Clicked: ", e.key);
@@ -57,11 +63,19 @@ const PostedJobsListings = () => {
     setOpen(true);
   };
   const handleOk = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant",
+    });
+    setSpinConnect(true);
+    console.log(idToUpdate);
+    setOpen(false);
     jobAPI
       .remove(idToUpdate)
       .then((response) => {
         console.log(response.data);
         setOpen(false);
+        setSpinConnect(false);
       })
       .catch((error) => {
         console.log(error);
@@ -70,6 +84,17 @@ const PostedJobsListings = () => {
   const handleCancel = () => {
     console.log("Clicked cancel button");
     setOpen(false);
+  };
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 8; // Số lượng mục trên mỗi trang
+  const totalItems = dataJob.length; // Tổng số mục trong danh sách của bạn
+  // Tính toán mục trên trang hiện tại
+  const startItemIndex = (currentPage - 1) * pageSize;
+  const endItemIndex = startItemIndex + pageSize;
+  const currentItems = dataJob.slice(startItemIndex, endItemIndex);
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
   };
 
   const menu = (
@@ -94,6 +119,27 @@ const PostedJobsListings = () => {
 
   return (
     <div className="listJobRecruiter">
+      {spinConnect ? (
+        <div
+          style={{
+            zIndex: "999",
+            width: "100%",
+            height: "100vh",
+            justifyContent: "center",
+            zIndex: 50,
+            marginTop: "20px",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            zIndex: 2,
+            position: "absolute",
+            right: "0px",
+            top: 0,
+          }}
+        >
+          <Spin indicator={antIcon} className="relative" />
+        </div>
+      ) : null}
       <div className="list">
         <div className="topic">
           <p>Job postings posted</p>
@@ -102,50 +148,76 @@ const PostedJobsListings = () => {
         <div className="content">
           <div className="Job">
             <div className="jobIcon">
-              {dataJob.map((value) => (
-                <div className="job" key={value.item}>
-                  <img
-                    src="https://static.topcv.vn/v4/image/logo/topcv-logo-6.png"
-                    alt=""
-                  />
-                  <div>
+              <List
+                className="123"
+                itemLayout="vertical"
+                pagination={{
+                  position: "center",
+                }}
+                style={{ gridTemplatecolumns: "50% 50%" }}
+                dataSource={dataJob}
+                renderItem={(value) => (
+                  <div className="job" key={value.item}>
+                    <img
+                      src="https://static.topcv.vn/v4/image/logo/topcv-logo-6.png"
+                      alt=""
+                    />
                     <div>
-                      <h5>{value.title}</h5>
-                      <div style={{ zIndex: "10" }}>
-                        <Dropdown overlay={menu}>
-                          <Button onMouseEnter={() => setIdToUpdate(value._id)}>
-                            <Settings />
-                          </Button>
-                        </Dropdown>
+                      <div>
+                        <h5 onClick={() => navigate(`/jobs/${value._id}`)}>
+                          {value.title.length > 40
+                            ? value.title.substr(0, 30) + "..."
+                            : value.title}
+                        </h5>
+                        <div style={{ zIndex: "10" }}>
+                          <Dropdown overlay={menu}>
+                            <Button
+                              onMouseEnter={() => setIdToUpdate(value._id)}
+                            >
+                              <Settings />
+                            </Button>
+                          </Dropdown>
+                        </div>
+                      </div>
+                      <h6>
+                        Position: <span>Full time</span>
+                      </h6>
+                      <div className="informationJob">
+                        <p>
+                          Salary:
+                          <span>
+                            {(() => {
+                              if (value.salary == "Negotiable") {
+                                return "Negotiable";
+                              }
+                              return `${value.salary} USD`;
+                            })()}
+                          </span>
+                        </p>
+                        <p>
+                          <MapPin />
+                          <span>Thành phố HCM</span>
+                        </p>
+                        <p>
+                          <CalendarCheck />
+                          Hạn Nộp: <span>30/12/2023</span>
+                        </p>
+                        <p style={{ width: "fit-content" }}>
+                          Status: <span>{value.status}</span>
+                        </p>
                       </div>
                     </div>
-                    <h6>
-                      Position: <span>Full time</span>
-                    </h6>
-                    <div className="informationJob">
-                      <p>
-                        Salary:
-                        <span>
-                          {(() => {
-                            if (value.salary == "Negotiable") {
-                              return "Negotiable";
-                            }
-                            return `${value.salary} USD`;
-                          })()}
-                        </span>
-                      </p>
-                      <p>
-                        <MapPin />
-                        <span>Thành phố HCM</span>
-                      </p>
-                      <p>
-                        <CalendarCheck />
-                        Hạn Nộp: <span>30/12/2023</span>
-                      </p>
-                    </div>
                   </div>
-                </div>
-              ))}
+                )}
+              />
+              <Pagination
+                style={{ marginTop: "10px", display: "flex" }}
+                current={currentPage}
+                pageSize={pageSize}
+                total={totalItems}
+                onChange={handlePageChange}
+                position="center"
+              />
             </div>
           </div>
         </div>
