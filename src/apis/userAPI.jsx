081f9a1f0dd540a0a25f9a2ddd1380e04c49148c
Trangna <trangna.ftu@gmail.com>
2023-11-06 01:38:23 +0700

    fix api

--- src/apis/userAPI.jsx
+++ src/apis/userAPI.jsx
@@ -2,29 +2,11 @@ import axiosInstance from "./axiosInstance";
 
 const userAPI = {
   // getById: () => axiosInstance.get("/users/getById"),
-  // getAll: (searchText, filterRole, currentPage, pageSize) => {
-  //   const queryParams = {};
-  //   if (searchText) {
-  //     queryParams.search = searchText;
-  //   }
-  //   if (filterRole) {
-  //     queryParams.roles = filterRole;
-  //   }
-  //   if (currentPage) {
-  //     queryParams.currentPage = currentPage;
-  //   }
-  //   if (pageSize) {
-  //     queryParams.pageSize = pageSize;
-  //   }
-  //   return axiosInstance.get(`/admin/users`, {
-  //     params: queryParams, 
-  //   });
-  // },  
-  // getAll: () => axiosInstance.get("/admin/users"),
-getAll : (values) => axiosInstance.get(
-  `/admin/users/?search=${values.search}&roles=${values.roles}&pageSize=${values.pageSize}&currentPage=${values.currentPage}`
+  getAll: (values) =>
+    axiosInstance.get(
+      `/admin/users/?search=${values.search}&roles=${values.roles}&pageSize=${values.pageSize}&currentPage=${values.currentPage}`
     ),
-  getAllRoles : () => axiosInstance.get("/role"),
+  getAllRoles: () => axiosInstance.get("/role"),
   create: (values) => axiosInstance.post("/users/create", values),
   update: (values) => axiosInstance.put("/users/update", values),
   changePassword: (values) =>
