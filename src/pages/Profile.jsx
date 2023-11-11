246700018cbd65449179c88a0e801363d9130d10
NhanDev <nhannguyenvh79@gmail.com>
2023-11-11 18:27:34 +0700

    css admin profile, profile modal

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,25 +3,41 @@ import { useState } from "react";
 import ProfileComponent from "../components/profile/ProfileComponent";
 import ChangePasswordModal from "../components/profile/ChangePasswordModal";
 import MasterLayout from "../components/layout/MasterLayout";
+import AuthContext from "../contexts/AuthContext/AuthContext";
+import { useContext } from "react";
+import AdminMasterLayout from "../components/layout/AdminMasterLayout";
 
 const Profile = () => {
   const [resetPasswordModalOpen, setResetPasswordModalOpen] = useState(false);
+  const { auth } = useContext(AuthContext);
   return (
     <>
-      <MasterLayout
-        ContentComponent={
-          <ProfileComponent
-            onOpenResetPasswordModal={() => setResetPasswordModalOpen(true)}
+      {auth.user.roleName === "admin" || auth.user.roleName === "superadmin" ? (
+        <AdminMasterLayout
+          ContentComponent={
+            <ProfileComponent
+              onOpenResetPasswordModal={() => setResetPasswordModalOpen(true)}
+            />
+          }
+        />
+      ) : (
+        <>
+          <MasterLayout
+            ContentComponent={
+              <ProfileComponent
+                onOpenResetPasswordModal={() => setResetPasswordModalOpen(true)}
+              />
+            }
+            hasSideBar={false}
+            hasFooter={true}
           />
-        }
-        hasSideBar={false}
-        hasFooter={true}
-      />
 
-      <ChangePasswordModal
-        show={resetPasswordModalOpen}
-        handleClose={() => setResetPasswordModalOpen(false)}
-      />
+          <ChangePasswordModal
+            show={resetPasswordModalOpen}
+            handleClose={() => setResetPasswordModalOpen(false)}
+          />
+        </>
+      )}
     </>
   );
 };
