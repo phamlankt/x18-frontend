eef4d2566a1d2486a34d0d31b9f98e8e393924ea
Lan Pham <lan.pham@dfs.de>
2023-11-24 06:47:08 +0100

    fix select gender

--- src/global/ModalFormItem.jsx
+++ src/global/ModalFormItem.jsx
@@ -1,45 +1,85 @@
 import { Field, ErrorMessage } from "formik";
+import { Select } from "antd";
+import React, { useState } from "react";
 
-function ModalFormItem({ item, isEditMode, errors, touched }) {
+function ModalFormItem({
+  item,
+  isEditMode,
+  errors,
+  touched,
+  setFieldTouched,
+  setFieldValue,
+}) {
+  const fieldName = item.fieldName;
+  const [value, setValue] = useState();
   return (
     <div className="form-row">
-      <div className="form-group col">
-        <label>
-          {item.label}
-          {item.isRequired && (
-            <span>
-              (<span className="text-danger">*</span>)
-            </span>
-          )}:
-        </label>
-        <Field
-          name={item.fieldName}
-          disabled={item.fieldName === "email" && isEditMode}
-          type={item.type && item.type}
-          as={item.as && item.as}
-          multiple={item.as && item.fieldName === "businessSector" && true}
-          size={item.as && item.fieldName === "businessSector" && 3}
-          rows={item.as && item.as === "textarea" && "3"}
-          className={
-            "form-control" +
-            (errors[item.fieldName] && touched[item.fieldName]
-              ? " is-invalid "
-              : "")
-          }
-        >
-          {item.options &&
-            item.options.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.name}
-              </option>
-            ))}
-        </Field>
-        <ErrorMessage
-          name={item.fieldName}
-          component="div"
-          className="invalid-feedback"
-        />
-      </div>
+      {item.as === "select" ? (
+        <div className="form-group col">
+          <label
+            htmlFor={item.fieldName}
+            onClick={() => setFieldTouched({ fieldName }, true)}
+          >
+            {item.label}
+          </label>
+
+          <Select
+            id={item.fieldName}
+            name={item.fieldName}
+            style={{
+              width: "100%",
+              backgroundColor: "white",
+              margin: " 4px 0px",
+              borderRadius: "4px",
+            }}
+            bordered={false}
+            allowClear={true}
+            placeholder={`Choose your ${item.fieldName}`}
+            onChange={(e) => {
+              setValue(e);
+              setFieldValue(item.fieldName, e.toLowerCase());
+            }}
+            value={value}
+            options={item.options.map((value) => ({
+              label: value.name,
+              value: value.name,
+            }))}
+          />
+          {errors[item.fieldName] && touched[item.fieldName] && (
+            <p className="text-danger form-error">{errors[item.fieldName]}</p>
+          )}
+        </div>
+      ) : (
+        <div className="form-group col">
+          <label>
+            {item.label}
+            {item.isRequired && (
+              <span>
+                (<span className="text-danger">*</span>)
+              </span>
+            )}
+            :
+          </label>
+          <Field
+            name={item.fieldName}
+            disabled={item.fieldName === "email" && isEditMode}
+            type={item.type && item.type}
+            as={item.as && item.as}
+            rows={item.as && item.as === "textarea" && "3"}
+            className={
+              "form-control" +
+              (errors[item.fieldName] && touched[item.fieldName]
+                ? " is-invalid "
+                : "")
+            }
+          />
+          <ErrorMessage
+            name={item.fieldName}
+            component="div"
+            className="invalid-feedback"
+          />
+        </div>
+      )}
     </div>
   );
 }
