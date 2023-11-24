21a33f82edb1c67b061a805be6c6920de543647c
Lan Pham <lan.pham@dfs.de>
2023-11-24 09:27:08 +0100

    clean up unused variables and files

--- src/global/fields/response.js
+++ src/global/fields/response.js
@@ -1,27 +1,3 @@
-const ResponseFields = {
-    data: "data",
-    messege: "messege",
-    catch: "catch",
-    error: "error",
-    cussor: "cussor",
-    did: "Did",
-    userInfo: "userInfo",
-    customerInfo: "customerInfo",
-    kiotInfo: "kiotInfo",
-    productInfo: "productInfo",
-    imageInfo: "imageInfo",
-    transactiontInfo: "transactiontInfo",
-    saleOffInfo: "saleOffInfo",
-    productList: "productList",
-    acceptToken: "acceptToken",
-    customerList: "customerList",
-    accountList: "accountList",
-    transactionList: "transactionList",
-    saleOffProductList: "saleOffProductList",
-    saleOffTransactionList: "saleOffTransactionList",
-    kiotList: "kiotList",
-    imageList:"imageList",
-    CodeMessage: "CodeMessage",
-}
+const ResponseFields = {};
 
 export { ResponseFields };
